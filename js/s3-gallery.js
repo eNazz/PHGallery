37ba45d5cd32e59d6dd2680997c9a0c222e8ab04
Esteban <esteban.nazer87@gmail.com>
2023-11-30 01:01:03 -0300

    nuevos cambios, ultimo intento de hoy

--- js/s3-gallery.js
+++ js/s3-gallery.js
@@ -4,46 +4,34 @@ AWS.config.update({
     secretAccessKey: 'XuCSFBsc8W/wGCpTtF8CfavyoaYghZ6UBX3/h4VE',
     region: 'us-east-1', // Ejemplo: 'us-east-1'
 });
-
-const s3 = new AWS.S3();
-const bucketName = 'tpfinal-python';
-
-// Función para cargar y mostrar imágenes desde S3
-async function loadImagesFromS3() {
+  
+  const s3 = new AWS.S3();
+  const bucketName = 'NOMBRE_DE_TU_BUCKET';
+  
+  async function loadImagesFromS3() {
     const galleryDiv = document.getElementById('s3-img-gallery');
-
+  
     try {
-        const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
-
-        // Limpiar la galería antes de agregar nuevas imágenes
-        galleryDiv.innerHTML = '';
-
-        // Recorrer objetos en el bucket y agregar imágenes a la galería
-        data.Contents.forEach((object) => {
-            const imageUrl = s3.getSignedUrl('getObject', { Bucket: bucketName, Key: object.Key });
-
-            const imgElement = document.createElement('img');
-            imgElement.src = imageUrl;
-            imgElement.alt = '';
-            imgElement.onclick = () => openFulImg(imageUrl); // Agregar función para mostrar imagen completa
-            galleryDiv.appendChild(imgElement);
-        });
+      const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
+  
+      // Limpiar la galería antes de agregar nuevas imágenes
+      galleryDiv.innerHTML = '';
+  
+      // Recorrer objetos en el bucket y agregar imágenes a la galería
+      data.Contents.forEach((object) => {
+        const imageUrl = s3.getSignedUrl('getObject', { Bucket: bucketName, Key: object.Key });
+  
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
+        imgElement.alt = '';
+        imgElement.onclick = () => openFulImg(imageUrl); // Agregar función para mostrar imagen completa
+        galleryDiv.appendChild(imgElement);
+      });
     } catch (error) {
-        console.error('Error al cargar imágenes desde S3:', error);
+      console.error('Error al cargar imágenes desde S3:', error);
     }
-}
-
-function openFulImg(reference) {
-    const fulImgBox = document.getElementById("fulImgBox");
-    const fulImg = document.getElementById("fulImg");
-
-    fulImgBox.style.display = "flex";
-    fulImg.src = reference;
-}
-
-function closeImg() {
-    const fulImgBox = document.getElementById("fulImgBox");
-    fulImgBox.style.display = "none";
-    
-// Llamar a la función para cargar imágenes al cargar la página
-window.onload = loadImagesFromS3;
+  }
+  
+  // Llamar a la función para cargar imágenes al cargar la página
+  window.onload = loadImagesFromS3;
+  
---
+++ js/subir-imagen.js
@@ -0,0 +1,47 @@
+// Configuración de AWS
+AWS.config.update({
+    accessKeyId: 'AKIA5NXILMPQJDZE4E2E',
+    secretAccessKey: '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',
+    region: 'us-east-1', // Ejemplo: 'us-east-1'
+});
+
+const s3 = new AWS.S3();
+const bucketName = 'NOMBRE_DE_TU_BUCKET';
+
+function uploadImages() {
+    const fileInput = document.getElementById('fileInput');
+    const files = fileInput.files;
+
+    if (files.length === 0) {
+        alert('Selecciona al menos una imagen.');
+        return;
+    }
+
+    const uploadPromises = [];
+
+    for (const file of files) {
+        const fileName = file.name;
+        const fileKey = 'uploads/' + fileName;
+
+        const params = {
+            Bucket: bucketName,
+            Key: fileKey,
+            Body: file,
+            ACL: 'public-read', // Esto hace que la imagen sea pública
+        };
+
+        uploadPromises.push(s3.upload(params).promise());
+    }
+
+    // Esperar a que todas las promesas de carga se completen
+    Promise.all(uploadPromises)
+        .then(() => {
+            alert('Imágenes subidas con éxito.');
+            // Recargar las imágenes desde S3
+            loadImagesFromS3();
+        })
+        .catch((error) => {
+            console.error('Error al subir imágenes:', error);
+            alert('Error al subir imágenes.');
+        });
+}
