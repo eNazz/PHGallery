6b508ae2da5059c7fdd819b047e7544cf7ec72ab
Esteban <esteban.nazer87@gmail.com>
2023-11-30 01:27:06 -0300

    Teesstttt

--- js/AWSConfig.js
+++
@@ -1,6 +0,0 @@
-// AWSConfig.js
-AWS.config.update({
-    accessKeyId: 'AKIA5NXILMPQJDZE4E2E',
-    secretAccessKey: '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',
-    region: 'us-east-1', // Ejemplo: 'us-east-1'
-});
--- js/s3-gallery.js
+++ js/s3-gallery.js
@@ -1,31 +1,51 @@
-import './AWSConfig.js'; // Importa la configuración de AWS
+// Configuración de AWS
+AWS.config.update({
+    accessKeyId: 'AKIA5NXILMPQJDZE4E2E',
+    secretAccessKey: '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',
+    region: 'us-east-1', // Ejemplo: 'us-east-1'
+});
 
+// Crear una nueva instancia de AWS.S3
 const s3 = new AWS.S3();
-const bucketName = 'tpfinal-python';
+const bucketName = 'NOMBRE_DE_TU_BUCKET';
 
-async function loadImagesFromS3() {
-    const galleryDiv = document.getElementById('s3-img-gallery');
+// Función para subir imágenes
+function uploadImages() {
+    const fileInput = document.getElementById('fileInput');
+    const files = fileInput.files;
 
-    try {
-        const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
-
-        // Limpiar la galería antes de agregar nuevas imágenes
-        galleryDiv.innerHTML = '';
-
-        // Recorrer objetos en el bucket y agregar imágenes a la galería
-        data.Contents.forEach((object) => {
-            const imageUrl = s3.getSignedUrl('getObject', { Bucket: bucketName, Key: object.Key });
-
-            const imgElement = document.createElement('img');
-            imgElement.src = imageUrl;
-            imgElement.alt = '';
-            imgElement.onclick = () => openFulImg(imageUrl); // Agregar función para mostrar imagen completa
-            galleryDiv.appendChild(imgElement);
-        });
-    } catch (error) {
-        console.error('Error al cargar imágenes desde S3:', error);
+    if (files.length === 0) {
+        alert('Selecciona al menos una imagen.');
+        return;
     }
+
+    const uploadPromises = [];
+
+    for (const file of files) {
+        const fileName = file.name;
+        const fileKey = 'uploads/' + fileName;
+
+        const params = {
+            Bucket: bucketName,
+            Key: fileKey,
+            Body: file,
+            ACL: 'public-read',
+        };
+
+        uploadPromises.push(s3.upload(params).promise());
+    }
+
+    Promise.all(uploadPromises)
+        .then(() => {
+            alert('Imágenes subidas con éxito.');
+            // Recargar las imágenes desde S3
+            loadImagesFromS3();
+        })
+        .catch((error) => {
+            console.error('Error al subir imágenes:', error);
+            alert('Error al subir imágenes.');
+        });
 }
 
-// Llamar a la función para cargar imágenes al cargar la página
+// Ejecutar la función de carga de imágenes desde S3 al cargar la página
 window.onload = loadImagesFromS3;
