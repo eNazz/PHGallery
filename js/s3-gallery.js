5506ffd59a224ea2cd8c4b289ce2d5df47a640b5
Esteban <esteban.nazer87@gmail.com>
2023-11-30 01:34:06 -0300

    val-bucket

--- js/s3-gallery.js
+++ js/s3-gallery.js
@@ -6,8 +6,8 @@ AWS.config.update({
 });
 
 // Crear una nueva instancia de AWS.S3
-const s3 = new AWS.S3();
 const bucketName = 'tpfinal-python';
+const s3 = new AWS.S3();
 
 // Función para subir imágenes
 function uploadImages() {
@@ -45,6 +45,31 @@ function uploadImages() {
             console.error('Error al subir imágenes:', error);
             alert('Error al subir imágenes.');
         });
+}
+
+// Función para cargar imágenes desde S3
+async function loadImagesFromS3() {
+    const galleryDiv = document.getElementById('s3-img-gallery');
+
+    try {
+        const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
+
+        // Limpiar la galería antes de agregar nuevas imágenes
+        galleryDiv.innerHTML = '';
+
+        // Recorrer objetos en el bucket y agregar imágenes a la galería
+        data.Contents.forEach((object) => {
+            const imageUrl = s3.getSignedUrl('getObject', { Bucket: bucketName, Key: object.Key });
+
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl;
+            imgElement.alt = '';
+            imgElement.onclick = () => openFulImg(imageUrl); // Agregar función para mostrar imagen completa
+            galleryDiv.appendChild(imgElement);
+        });
+    } catch (error) {
+        console.error('Error al cargar imágenes desde S3:', error);
+    }
 }
 
 // Ejecutar la función de carga de imágenes desde S3 al cargar la página
